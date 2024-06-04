a3dd8af1bf15e7c2eef40c8350827e5f737845b5
K <muhammed-fazil.k@capgemini.com>
2024-06-05 01:30:49 +0530

    Add streaming for data fetching

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const page = () => {
-  return <div>invoice page</div>;
+const page = async() => {
+  const data = await new Promise(res => setTimeout(()=>res('Resolved'),4000))
+  return <div>invoice page {data}</div>;
 };
 
 export default page;
